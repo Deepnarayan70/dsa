6e9700930e4e95b7e7b4c2a14296dafda5b4aedf
Deep Narayan <deepnaryan03@gmail.com>
2026-01-29 22:47:18 +0530

    update

--- day6/AggressiveCows.cpp
+++ day6/AggressiveCows.cpp
@@ -6,6 +6,77 @@
 
 using namespace std;
 
+int maximize_the_min_distance(int arr[], int n, int k)
+{
+    int ans = INT_MIN;
+
+    for (int i = 1; i < (1 << n); i++)
+    {
+        int count = 0;
+        int min_dis = INT_MAX;
+        int last = -1;
+    
+        for (int j = 0; j < (1 << n); j++)
+        {
+            if ((i & (1 << j)) != 0)
+            {   
+                count++;
+                if(last == -1){
+                    last = arr[j];
+                }
+                else{
+                    min_dis = min(min_dis, arr[j] - last);
+                    last = arr[j];
+                }
+            }
+        }
+
+        if(count == k && min_dis != INT_MAX){
+            ans = max(ans, min_dis);
+        }
+    }
+    return ans;
+}
+
+//#binary search approach
+bool isPossible(vector<int>arr, int n, int d, int k){
+     int count = 1;
+     int last_pos = arr[0];
+
+     for(int i = 1; i < n; i++){
+        if(arr[i] - last_pos >= d){
+            count++;
+            last_pos = arr[i];
+        }
+
+        if(count == k){
+            return true;
+        }
+     }
+     return false;   
+}
+
+// O(n*log2(10^9))
+int agg_cows(vector<int> arr, int n, int k){
+    int s = 1;
+    int e = arr[n - 1] - arr[0];
+    int ans = -1;
+
+    while(s <= e){
+        int mid = s + (e - s) / 2;
+
+        if(isPossible(arr, n, mid, k)){
+            s = mid + 1;
+            ans = mid;
+        }
+        else{
+            e = mid - 1;
+        }
+    }
+    return ans;
+}
+
+
 int main(){
     int n; cin>>n;
     vector<int> arr(n);
@@ -16,24 +87,27 @@ int main(){
     cin>>k;
     sort(arr.begin(),arr.end());
 
-    //#Brute force approach
-    //for printing subsequence of array
-    for(int i=0;i<(1<<n);i++){
-        vector<int> temp;
-        for(int j=0;j<n;j++){
-            if((i & (1<<j))!=0){
-                temp.push_back(arr[j]);
-            }
-        }
-        if(temp.size()==k){
-            for(int a:temp){
-                cout<<a<<" ";
-            }
-            cout<<endl;
-        }
-    }
+    //cout << maximize_the_min_distance(arr, n, k) << '\n';
+    // //#Brute force approach
+    // //for printing subsequence of array
+    // for(int i=0;i<(1<<n);i++){
+    //     vector<int> temp;
+    //     for(int j=0;j<n;j++){
+    //         if((i & (1<<j))!=0){
+    //             temp.push_back(arr[j]);
+    //         }
+    //     }
+    //     if(temp.size()==k){
+    //         for(int a:temp){
+    //             cout<<a<<" ";
+    //         }
+    //         cout<<endl;
+    //     }
+    // }
 
     //#binary search approach
+    
+    cout<< agg_cows(arr,n,k);
 }
  
 
