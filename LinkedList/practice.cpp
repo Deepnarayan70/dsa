100a7e444a28380e1e758a702418092dabdee722
Deep Narayan <deepnaryan03@gmail.com>
2026-02-09 11:24:45 +0530

    Updated

--- LinkedList/practice.cpp
+++ LinkedList/practice.cpp
@@ -156,3 +156,110 @@ using namespace std;
 // }
 
  
+//Leetcode problem 19 ->Done
+// 19. Remove Nth Node From End of List
+// Solved
+// Medium
+// Topics
+// premium lock icon
+// Companies
+// Hint
+// Given the head of a linked list, remove the nth node from the end of the list and return its head.
+// Example 1:
+// Input: head = [1,2,3,4,5], n = 2
+// Output: [1,2,3,5]
+// Example 2:
+// Input: head = [1], n = 1
+// Output: []
+// Example 3:
+// Input: head = [1,2], n = 1
+// Output: [1]
+// /**
+//  * Definition for singly-linked list.
+//  * struct ListNode {
+//  *     int val;
+//  *     ListNode *next;
+//  *     ListNode() : val(0), next(nullptr) {}
+//  *     ListNode(int x) : val(x), next(nullptr) {}
+//  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+//  * };
+//  */
+
+// class Solution {
+// public:
+//     int size(ListNode* head){
+//         int s=0;
+//         ListNode* temp=head;
+//         while(temp!=NULL){
+//             s++;
+//             temp=temp->next;
+//         }
+//         return s;
+//     }
+//     ListNode* removeNthFromEnd(ListNode* head, int n) {
+//         int s= size(head);
+//         ListNode* temp=head;
+//         if(s==n) {
+//             head=head->next;
+//             return head;
+//             }
+
+//         for(int i=0;i<s-n-1;i++){
+//             temp=temp->next;
+//         }
+ 
+//         ListNode* del = temp->next;
+//         temp->next = del->next;
+//         delete del;
+ 
+//         return head;
+//     }
+// };
+
+// //Leetcode 61
+// 61. Rotate List
+// Solved
+// Medium
+// Topics
+// premium lock icon
+// Companies
+// Given the head of a linked list, rotate the list to the right by k places.
+// Example 1:
+// Input: head = [1,2,3,4,5], k = 2
+// Output: [4,5,1,2,3]
+// Example 2:
+// Input: head = [0,1,2], k = 4
+// Output: [2,0,1]
+// /**
+//  * Definition for singly-linked list.
+//  * struct ListNode {
+//  *     int val;
+//  *     ListNode *next;
+//  *     ListNode() : val(0), next(nullptr) {}
+//  *     ListNode(int x) : val(x), next(nullptr) {}
+//  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+//  * };
+//  */
+// class Solution {
+// public:
+//     ListNode* rotateRight(ListNode* head, int k) {
+//         ListNode* temp=head;
+//         int s=1;
+//         if(head==NULL || head->next==NULL || k==0) return head;
+//         while(temp->next!=NULL){
+//             temp=temp->next;
+//             s++;
+//         }
+//         temp->next=head;
+//         s-=(k%s);
+//         temp=head;
+ 
+//         while(s>1){
+//             temp=temp->next;
+//             s--;
+//         }
+//         head=temp->next;
+//         temp->next=NULL;
+//         return head;
+//     }
+// };
