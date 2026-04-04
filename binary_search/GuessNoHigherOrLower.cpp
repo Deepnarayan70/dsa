e2bdccf95bd6479fcbc0de69a8f3db33225a098a
Deep Narayan <deepnaryan03@gmail.com>
2026-04-05 01:32:57 +0530

    changes

---
+++ binary_search/GuessNoHigherOrLower.cpp
@@ -0,0 +1,44 @@
+// 374. Guess Number Higher or Lower
+// Solved
+// Easy
+// Topics
+// premium lock icon
+// Companies
+// We are playing the Guess Game. The game is as follows:
+// I pick a number from 1 to n. You have to guess which number I picked (the number I picked stays the same throughout the game).
+// Every time you guess wrong, I will tell you whether the number I picked is higher or lower than your guess.
+// You call a pre-defined API int guess(int num), which returns three possible results:
+// -1: Your guess is higher than the number I picked (i.e. num > pick).
+// 1: Your guess is lower than the number I picked (i.e. num < pick).
+// 0: your guess is equal to the number I picked (i.e. num == pick).
+// Return the number that I picked.
+// Example 1:
+// Input: n = 10, pick = 6
+// Output: 6
+// Example 2:
+// Input: n = 1, pick = 1
+// Output: 1
+// Example 3:
+// Input: n = 2, pick = 1
+// Output: 1
+ 
+
+
+
+// class Solution {
+// public:
+//     int guessNumber(int n) {
+//          return binarySearch(1, n);
+//  }
+                        
+//  private:
+//  int binarySearch(int left, int right) {
+//        int mid = left + (right - left) / 2;
+//        int result = guess(mid);
+                                                    
+//        if (result == 0) return mid;
+//        if (result == -1) return binarySearch(left, mid - 1);
+//              return binarySearch(mid + 1, right);
+//     }
+ 
+// };
