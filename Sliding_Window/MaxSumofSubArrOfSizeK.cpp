14fbd20f5fea80d44538af17bece96a3e94bbb2d
Deep Narayan <deepnaryan03@gmail.com>
2026-06-24 15:02:21 +0530

    Updates

---
+++ Sliding_Window/MaxSumofSubArrOfSizeK.cpp
@@ -0,0 +1,37 @@
+// Max Sum Subarray of size K
+// Difficulty: EasyAccuracy: 49.6%Submissions: 299K+Points: 2
+// Given an array of integers arr[]  and a number k. Return the maximum sum of a subarray of size k.
+
+// Note: A subarray is a contiguous part of any given array.
+
+// Examples:
+
+// Input: arr[] = [100, 200, 300, 400], k = 2
+// Output: 700
+// Explanation: arr2 + arr3 = 700, which is maximum.
+// Input: arr[] = [1, 4, 2, 10, 23, 3, 1, 0, 20], k = 4
+// Output: 39
+// Explanation: arr1 + arr2 + arr3 + arr4 = 39, which is maximum.
+// Input: arr[] = [100, 200, 300, 400], k = 1
+// Output: 400
+// Explanation: arr3 = 400, which is maximum.
+// Constraints:
+// 1 ≤ arr.size() ≤ 106
+// 0 ≤ arr[i] ≤ 106
+// 1 ≤ k ≤ arr.size()
+
+
+// class Solution {
+//   public:
+//     int maxSubarraySum(vector<int>& arr, int k) {
+//         // code here
+//  	int sum=0;
+// 	for(int i=0;i<k;i++) sum+=arr[i];
+// 	int val=sum;
+// 	for(int i=k;i<arr.size();i++){
+// 		val= val+arr[i]-arr[i-k];
+// 		sum=max(sum,val);
+// 	}
+// 	return sum;
+//     }
+// };
